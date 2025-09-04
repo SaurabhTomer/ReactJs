48938b3d2bcb1ffbc91535323e43be14bf94dee2
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-09-04 17:54:52 +0530

    Your Logo

---
+++ 07ReactRouter/src/Layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+// import Header from './components/Header/header'
+import Header from './components/Header/header'
+import Footer from './components/Footer/footer'
+
+
+function Layout() {
+  return (
+    <>
+
+    <Header/>
+    <Outlet/>
+    <Footer/>
+    
+    </>
+  )
+}
+
+export default Layout
